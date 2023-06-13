096fd77547c80d6366db74a2487f42cd3ceed187
subba reddy <subbareddy63047@example.com>
2023-06-13 11:56:26 +0000

    Authentication and Autherization part-2 class-code

---
+++ src/components/Cart/index.js
@@ -0,0 +1,20 @@
+// Write your JS code here
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+        alt="cart"
+        className="product-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
